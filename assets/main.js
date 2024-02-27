f05244e41ca330447b40045e3e97d91810c40ec1
Vialsiu <D00265675@student.dkit.ie>
2024-02-27 11:13:52 +0000

    fixed displaying table for almond milk

--- assets/main.js
+++ assets/main.js
@@ -27,21 +27,21 @@ fetch('https://derek.comp.dkit.ie/java_script/example_code/food.json')
       htmlString += `<tr>
                         <td>${food.id}</td>
                         <td>${food.name}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].energy ? food['nutrition-per-100g'].energy : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].protein ? food['nutrition-per-100g'].protein : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g']['saturated-fat'] ? food['nutrition-per-100g']['saturated-fat'] : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g']['trans-fat'] ? food['nutrition-per-100g']['trans-fat'] : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g']['polyunsaturated-fat'] ? food['nutrition-per-100g']['polyunsaturated-fat'] : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g']['monounsaturated-fat'] ? food['nutrition-per-100g']['monounsaturated-fat'] : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].carbohydrate ? food['nutrition-per-100g'].carbohydrate : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].sugars ? food['nutrition-per-100g'].sugars : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g']['dietary-fibre'] ? food['nutrition-per-100g']['dietary-fibre'] : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].sodium ? food['nutrition-per-100g'].sodium : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].potassium ? food['nutrition-per-100g'].potassium : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].calcium ? food['nutrition-per-100g'].calcium : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g']['vitamin-e'] ? food['nutrition-per-100g']['vitamin-e'] : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].fat ? food['nutrition-per-100g'].fat : '-'}</td>
-                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].magnesium ? food['nutrition-per-100g'].magnesium : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].energy? food['nutrition-per-100g'].energy : food['nutrition-per-100ml'] && food['nutrition-per-100ml'].energy? food['nutrition-per-100ml'].energy : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].protein? food['nutrition-per-100g'].protein : food['nutrition-per-100ml'] && food['nutrition-per-100ml'].protein? food['nutrition-per-100ml'].protein : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g']['saturated-fat']? food['nutrition-per-100g']['saturated-fat'] : food['nutrition-per-100ml'] && food['nutrition-per-100ml']['saturated-fat']? food['nutrition-per-100ml']['saturated-fat'] : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g']['trans-fat']? food['nutrition-per-100g']['trans-fat'] : food['nutrition-per-100ml'] && food['nutrition-per-100ml']['trans-fat']? food['nutrition-per-100ml']['trans-fat'] : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g']['polyunsaturated-fat']? food['nutrition-per-100g']['polyunsaturated-fat'] : food['nutrition-per-100ml'] && food['nutrition-per-100ml']['polyunsaturated-fat']? food['nutrition-per-100ml']['polyunsaturated-fat'] : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g']['mononunsaturated-fat']? food['nutrition-per-100g']['monounsaturated-fat'] : food['nutrition-per-100ml'] && food['nutrition-per-100ml']['monounsaturated-fat']? food['nutrition-per-100ml']['monounsaturated-fat'] : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].carbohydrate? food['nutrition-per-100g'].carbohydrate : food['nutrition-per-100ml'] && food['nutrition-per-100ml'].carbohydrate? food['nutrition-per-100ml'].carbohydrate : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].sugars? food['nutrition-per-100g'].sugars : food['nutrition-per-100ml'] && food['nutrition-per-100ml'].sugars? food['nutrition-per-100ml'].sugars : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g']['dietary-fibre']? food['nutrition-per-100g']['dietary-fibre'] : food['nutrition-per-100ml'] && food['nutrition-per-100ml']['dietary-fibre']? food['nutrition-per-100ml']['dietary-fibre'] : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].sodium? food['nutrition-per-100g'].sodium : food['nutrition-per-100ml'] && food['nutrition-per-100ml'].sodium? food['nutrition-per-100ml'].sodium : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].potassium? food['nutrition-per-100g'].potassium : food['nutrition-per-100ml'] && food['nutrition-per-100ml'].potassium? food['nutrition-per-100ml'].potassium : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].calcium? food['nutrition-per-100g'].calcium : food['nutrition-per-100ml'] && food['nutrition-per-100ml'].calcium? food['nutrition-per-100ml'].calcium : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g']['vitamin-e']? food['nutrition-per-100g']['vitamin-e'] : food['nutrition-per-100ml'] && food['nutrition-per-100ml']['vitamin-e']? food['nutrition-per-100ml']['vitamin-e'] : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].fat? food['nutrition-per-100g'].fat : food['nutrition-per-100ml'] && food['nutrition-per-100ml'].fat? food['nutrition-per-100ml'].fat : '-'}</td>
+                        <td>${food['nutrition-per-100g'] && food['nutrition-per-100g'].magnesium? food['nutrition-per-100g'].magnesium : food['nutrition-per-100ml'] && food['nutrition-per-100ml'].magnesium? food['nutrition-per-100ml'].magnesium : '-'}</td>
                     </tr>`;
     });
 
